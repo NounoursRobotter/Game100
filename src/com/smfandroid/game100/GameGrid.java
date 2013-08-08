23d03e5fd8125e8c3fdfc4492b3e2f359c196817
Simon M <webaccount@marache.net>
2013-08-08 23:28:19 +0200

    New (and better) touch interface

--- src/com/smfandroid/game100/GameGrid.java
+++ src/com/smfandroid/game100/GameGrid.java
@@ -1,70 +1,86 @@
 package com.smfandroid.game100;
 
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Random;
 
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Point;
-import android.support.v4.view.VelocityTrackerCompat;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
-import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.smfandroid.game100.C100GameCore.IllegalMoveException;
 
-public class GameGrid extends LinearLayout implements OnTouchListener {
+public class GameGrid extends LinearLayout {
+
 
 	protected class GridElement extends TextView {
-		protected int mValue;
+		protected class MyValue {
+			protected int mValue;
+		}
+		MyValue m = new MyValue();
+
+		protected Point mCurrentPoint;
+		
+		protected class MyElementClickListener implements OnClickListener {
+			protected Point mPoint;
+			public MyElementClickListener(Point p) {
+				mPoint = p;
+			}
+			@Override
+			public void onClick(View v) {
+				if(m.mValue == -2)
+					nextPointSelected(mPoint);
+			}
+		}
 		
 		public int getValue() {
-			return mValue;
+			return m.mValue;
 		}
 		
 		public void setEmptyValue() {
-			mValue = -1;
+			m.mValue = -1;
 			updateValue();
 		}
 
 		public void setPossibleValue() {
-			mValue = -2;
+			m.mValue = -2;
 			updateValue();
+
 		}
 		
 		public void setValue(int value) {
 			if(value <=0)
 				throw new IllegalArgumentException("The value is a stricly positive integer");
 			
-			this.mValue = value;
+			m.mValue = value;
 			updateValue();
 		}
 
 		private void updateValue() {
-			if(mValue > 0)
-				setText(Integer.toString(mValue));
+			if(m.mValue > 0)
+				setText(Integer.toString(m.mValue));
 			else
 				setText(" ");
-			if(mValue == -2)
+			if(m.mValue == -2)
 				setBackgroundColor(Color.GREEN);
 			else
 				setBackgroundColor(Color.WHITE);
 		}
 		
-		public GridElement(Context context) {
+		public GridElement(Context context, Point p) {
 			super(context);
+			this.mCurrentPoint = p;
 			this.setBackgroundColor(Color.WHITE);
 			this.setWidth(0);
 			this.setGravity(Gravity.CENTER);
+			this.setOnClickListener( new MyElementClickListener(mCurrentPoint) );
+
 			setEmptyValue();
+			
 		}
 		
 	}
@@ -75,8 +91,6 @@ public class GameGrid extends LinearLayout implements OnTouchListener {
 	private Random mRndGenerator = new Random();
 	private C100GameCore mGameCore;
 	
-	private VelocityTracker mVelocityTracker = null;
-
 	public void setSize(int nbRows, int nbColumns) {
 		if(nbRows <= 0 || nbRows <=0)
 			throw new IllegalArgumentException("No size value can be zero or less");
@@ -95,7 +109,7 @@ public class GameGrid extends LinearLayout implements OnTouchListener {
 		return mNbColumns;
 	}
 
-	protected View CreateRow() {
+	protected View CreateRow(int numRow) {
 		LinearLayout row = new LinearLayout(getContext());
 		row.setOrientation(LinearLayout.HORIZONTAL);
 
@@ -103,7 +117,7 @@ public class GameGrid extends LinearLayout implements OnTouchListener {
 		layoutElement.setMargins(2, 2, 2, 2);
 
 		for(int j = 0; j < getNbColumns(); j++) {
-			row.addView(new GridElement(getContext()), layoutElement);
+			row.addView(new GridElement(getContext(), new Point(j, numRow)), layoutElement);
 		}		
 		row.setGravity(Gravity.CENTER);
 		return row;
@@ -122,13 +136,15 @@ public class GameGrid extends LinearLayout implements OnTouchListener {
 		redrawValues();
 	}
 	
+
 	protected void setValueAt(Point position, int value) {
 		LinearLayout l = (LinearLayout)getChildAt(position.y);
 		GridElement gridE = (GridElement)l.getChildAt(position.x);
 		if(value == EMPTY_VALUE)
 			gridE.setEmptyValue();
-		else if(value == POSSIBLE_VALUE)
+		else if(value == POSSIBLE_VALUE) {
 			gridE.setPossibleValue();
+		}
 		else			
 			gridE.setValue(value);
 	}
@@ -156,14 +172,8 @@ public class GameGrid extends LinearLayout implements OnTouchListener {
 	}
 	
 	public void nextPointSelected(Point p) {
-		Point oldPoint, newPoint;
-		LinkedList<Point> lMoves = mGameCore.GetState();
-		oldPoint = lMoves.getLast();
-		newPoint = new Point();
-		newPoint.x =  oldPoint.x + p.x;
-		newPoint.y += oldPoint.y + p.y;
 		try {
-			mGameCore.PushMove(newPoint);
+			mGameCore.PushMove(p);
 			redrawValues();
 		} catch (IllegalMoveException e) {
 			Toast.makeText(getContext(), R.string.msg_illegal_move, Toast.LENGTH_SHORT).show();
@@ -178,7 +188,7 @@ public class GameGrid extends LinearLayout implements OnTouchListener {
 		LayoutParams layoutRow = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, 1.0f);
 
 		for(int i = 0; i< getNbRows(); i++) {
-			this.addView(CreateRow(), layoutRow);
+			this.addView(CreateRow(i), layoutRow);
 		}
 	}	
 
@@ -186,75 +196,7 @@ public class GameGrid extends LinearLayout implements OnTouchListener {
 		super(context, attrs);
 		this.setBackgroundColor(Color.BLACK);
 		setSize(10, 10);		
-		setOnTouchListener(this);
 		reset();
 	}
-
-	@Override
-	public boolean onTouch(View arg0, MotionEvent event) {
-		Log.i(TAG, "Touch detected");
-		int index = event.getActionIndex();
-		int action = event.getActionMasked();
-		int pointerId = event.getPointerId(index);
-
-		switch(action) {
-		case MotionEvent.ACTION_DOWN:
-			if(mVelocityTracker == null) {
-				// Retrieve a new VelocityTracker object to watch the velocity of a motion.
-				mVelocityTracker = VelocityTracker.obtain();
-			}
-			else {
-				// Reset the velocity tracker back to its initial state.
-				mVelocityTracker.clear();
-			}
-			// Add a user's movement to the tracker.
-			mVelocityTracker.addMovement(event);
-			break;
-		case MotionEvent.ACTION_MOVE:
-			mVelocityTracker.addMovement(event);
-			break;
-		case MotionEvent.ACTION_UP:
-			Point nextPoint = new Point();
-			// When you want to determine the velocity, call 
-			// computeCurrentVelocity(). Then call getXVelocity() 
-			// and getYVelocity() to retrieve the velocity for each pointer ID. 
-			mVelocityTracker.computeCurrentVelocity(1000);
-			// Log velocity of pixels per second
-			// Best practice to use VelocityTrackerCompat where possible.
-			float x = VelocityTrackerCompat.getXVelocity(mVelocityTracker, pointerId);
-			float y = VelocityTrackerCompat.getYVelocity(mVelocityTracker, pointerId);
-
-			// Clearly one direction is stronger than the other
-			if(Math.abs(x) > 2 * Math.abs(y)) {
-				if(x > 0)
-					nextPoint = new Point(3,0);
-				else
-					nextPoint = new Point(-3,0);
-			} else if(Math.abs(x) < 0.5 * Math.abs(y)) {
-				if(y > 0)
-					nextPoint = new Point(0,3);
-				else
-					nextPoint = new Point(0,-3);
-				// Diagonals
-			} else if(x > 0) { // X > 0
-			if(y > 0)
-				nextPoint = new Point(2,2);
-			else
-				nextPoint = new Point(2,-2);
-			} else             // X < 0
-				if (y > 0)
-					nextPoint = new Point(-2,2);
-				else
-					nextPoint = new Point(-2,-2);
-			nextPointSelected(nextPoint);
-		case MotionEvent.ACTION_CANCEL:
-			// Return a VelocityTracker object back to be re-used by others.
-			mVelocityTracker.recycle();
-			break;
-		}
-		return true;
-	}
-
-
 	
 }
